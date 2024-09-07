e9ef9ae3618fd03711fdecf586ad4d7bcc8511b5
aghendaanas <anas.gh03@gmail.com>
2024-09-07 12:58:46 +0200

    Initial commit

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -3,10 +3,15 @@ import './AdBanner.css';
 
 // Import your images
 import ads1 from '../assets/ads1.png';
-import ads2 from '../assets/ads2.jpg';
-import ads3 from '../assets/ads3.jpg';
+import ads2 from '../assets/ads2.png';
+import ads3 from '../assets/ads3.png';
+import ads4 from '../assets/ads4.png';
+import ads5 from '../assets/ads5.png';
+import ads6 from '../assets/ads6.png';
+import ads7 from '../assets/ads7.png';
+import ads8 from '../assets/ads8.png';
 
-const images = [ads1, ads2, ads3];
+const images = [ads1, ads2, ads3 , ads4 , ads5 ,ads6, ads7 , ads8];
 
 const AdBanner = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -16,7 +21,7 @@ const AdBanner = () => {
             setCurrentIndex((prevIndex) =>
                 prevIndex === images.length - 1 ? 0 : prevIndex + 1
             );
-        }, 3000); // Change image every 3 seconds
+        }, 6000); // Change image every 3 seconds
 
         return () => clearInterval(interval); // Clean up interval on component unmount
     }, []);
